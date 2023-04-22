4ac089ef4f50ecf4f0aba8d82d33d6941179073d
Keanu <k.ruddy.mine@gmail.com>
2023-04-22 22:48:13 +0300

    Week01 - Name Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string grade;
+
+        if (gradePercentage >= 90) {
+            grade = "A";
+        }
+        else if (gradePercentage >= 80) {
+            grade = "B";
+        }
+        else if (gradePercentage >= 70) {
+            grade = "C";
+        }
+        else if (gradePercentage >= 60) {
+            grade = "D";
+        }
+        else {
+            grade = "F";
+        }
+
+        Console.WriteLine("Your grade is " + grade);
+        
+        if (gradePercentage >= 70) {
+            Console.WriteLine("Congratulation, you passed!");
+        }
+        else {
+            Console.WriteLine("Unfortunately, you didn't pass, you'll do better next time");
+        }
     }
+
 }
