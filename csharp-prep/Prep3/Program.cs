aff6ed71ef7967e0a1732b8fc30e67dea8c1eff7
Keanu <k.ruddy.mine@gmail.com>
2023-04-25 22:39:16 +0300

    Prep 3 Week 02

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess;
+        int counter = 0;
+
+        do {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            counter++;
+
+            if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            } else if (guess > magicNumber) {
+                Console.WriteLine("Lower");
+            } else {
+                break;
+            }
+        } while (guess != magicNumber);
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"With {counter} guesses");
     }
 }
