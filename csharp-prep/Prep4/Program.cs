972fdbca16d36e51a9d451fedfb705d6ee5934b1
Keanu <k.ruddy.mine@gmail.com>
2023-04-26 22:15:45 +0300

    Lists Week 02

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = 1;
+        List<int> listNumber = new List<int>();
+        int sum = 0;
+
+        while (number != 0) {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number == 0) break;
+            listNumber.Add(number);
+        }
+        foreach (int i in listNumber) sum += i;
+        double average = (double)sum / listNumber.Count();
+        int max = 0;
+        foreach (int i in listNumber) {
+            if (max < i) max = i;
+        }
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + max);
+    }   
 }
