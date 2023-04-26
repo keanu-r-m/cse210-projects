439b74291e5ed04b7e0f5082d29b7affc6c7d505
Keanu <k.ruddy.mine@gmail.com>
2023-04-26 22:48:26 +0300

    functions Week 02

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(favNumber);
+        DisplayResult(userName, squareNumber);
     }
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the program!");   
+    }
+    static string PromptUserName() {
+        Console.Write("please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber() {
+        Console.Write("please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number) {
+        return number * number;
+    }
+    static void DisplayResult(string name, int squared) {
+        Console.WriteLine($"{name}, the square is {squared}");
+    }
+
+
 }
